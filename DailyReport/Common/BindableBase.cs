1a628f9cdac84df55a34eae5364f233059ff1d4d
gnk263 <gnk263@users.noreply.github.com>
2018-05-30 01:17:45 +0900

    BindableBaseクラスを追加

---
+++ DailyReport/Common/BindableBase.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using DailyReport.Annotations;
+
+namespace DailyReport.Common
+{
+    public class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void SetProperty([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
