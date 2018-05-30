74181b6c82b401edf574c833949d51205cddf248
gnk263 <gnk263@users.noreply.github.com>
2018-05-30 09:33:35 +0900

    MainWindowViewModelを追加

---
+++ DailyReport/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,21 @@
+﻿using DailyReport.Common;
+using Reactive.Bindings;
+
+namespace DailyReport.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        public ReactiveProperty<string> OutputPath { get; }
+        public ReactiveProperty<string> Name { get; }
+        public ReactiveProperty<string> Title { get; }
+        public ReactiveProperty<string> Body { get; }
+
+        public ReactiveCommand SaveTempCommand { get; }
+        public ReactiveCommand SaveCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            
+        }
+    }
+}
