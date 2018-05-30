2ebf645959f0633904291fc5e4cbdbabf84ec86e
gnk263 <gnk263@users.noreply.github.com>
2018-05-30 09:43:14 +0900

    入力値の必須確認を実装

--- DailyReport/ViewModels/MainWindowViewModel.cs
+++ DailyReport/ViewModels/MainWindowViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
 using System.Reactive.Linq;
 using DailyReport.Common;
 using Reactive.Bindings;
@@ -7,9 +8,16 @@ namespace DailyReport.ViewModels
 {
     public class MainWindowViewModel : BindableBase
     {
+        [Required(ErrorMessage = "格納場所を入力してください。")]
         public ReactiveProperty<string> OutputPath { get; }
+
+        [Required(ErrorMessage = "名前を入力してください。")]
         public ReactiveProperty<string> Name { get; }
+
+        [Required(ErrorMessage = "タイトルを入力してください。")]
         public ReactiveProperty<string> Title { get; }
+
+        [Required(ErrorMessage = "内容を入力してください。")]
         public ReactiveProperty<string> Body { get; }
 
         public ReactiveCommand SaveTempCommand { get; }
@@ -17,10 +25,17 @@ namespace DailyReport.ViewModels
 
         public MainWindowViewModel()
         {
-            OutputPath = new ReactiveProperty<string>();
-            Name = new ReactiveProperty<string>();
-            Title = new ReactiveProperty<string>();
-            Body = new ReactiveProperty<string>();
+            OutputPath = new ReactiveProperty<string>()
+                .SetValidateAttribute(() => OutputPath);
+
+            Name = new ReactiveProperty<string>()
+                .SetValidateAttribute(() => Name);
+
+            Title = new ReactiveProperty<string>()
+                .SetValidateAttribute(() => Title);
+
+            Body = new ReactiveProperty<string>()
+                .SetValidateAttribute(() => Body);
 
             SaveTempCommand = new ReactiveCommand();
 
